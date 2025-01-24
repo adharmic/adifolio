708fc5f22fc40658691cb16e7d372050616a4b76
Adithya Ajith <adithya@satyaloka.org>
2025-01-24 13:24:55 -0600

    Fix navbar and add gradient to background

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,21 @@ import Experience from './sections/Experience'
 
 export default function App () {
   return (
-    <div className='w-screen h-min-screen flex items-center font-[200] flex-col'>
+    <>
       <Navbar />
-      <div className='w-11/12 md:w-3/4 flex flex-col gap-8 pt-16'>
-        <Home />
-        <Separator />
-        <Experience />
-        {/* <div className='flex flex-row gap-16'>
+      <div className='gradient w-screen h-min-screen flex items-center font-[200] flex-col'>
+        <div className='w-11/12 md:w-3/4 flex flex-col gap-8 pt-16'>
+          <Home />
+          <Separator />
+          <Experience />
+          {/* <div className='flex flex-row gap-16'>
           <div className='w-[200px]'>
             <div className='w-full text-2xl'>Projects</div>
           </div>
           <div className='w-full'></div>
         </div> */}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
