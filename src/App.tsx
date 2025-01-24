86ccc399993af6a7a0d7f5c9143f501e3024f536
Adithya Ajith <adithya@satyaloka.org>
2025-01-24 13:04:21 -0600

    Increase width on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App () {
   return (
     <div className='w-screen h-min-screen flex items-center font-[200] flex-col'>
       <Navbar />
-      <div className='w-3/4 flex flex-col gap-8 pt-16'>
+      <div className='w-11/12 md:w-3/4 flex flex-col gap-8 pt-16'>
         <Home />
         <Separator />
         <Experience />
