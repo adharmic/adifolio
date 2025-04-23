7053504c4f4dc03ad67a1f14923326b245c27149
Adithya Ajith <adithya@satyaloka.org>
2025-04-23 17:17:54 +0530

    Adjust font weights for hierarchy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react'
 export default function Header(props: PropsWithChildren) {
   return (
     <div className='w-full md:w-[200px]'>
-      <div className='w-full text-2xl md:text-start font-[300] gradient-header'>
+      <div className='w-full text-2xl md:text-start font-[400] gradient-header'>
         {props.children}
       </div>
     </div>
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -18,8 +18,8 @@ export default function Work({
       <div className="p-2 rounded-full bg-[#ffffe3] w-20 aspect-square">
         <img src={logo} className="w-20 aspect-square object-contain bg-[#ffffe3]" />
       </div>
-      <div className='w-full text-2xl font-[400]'>{company}</div>
-      <div className='w-full text-xl font-[300]'>{position}</div>
+      <div className='w-full text-2xl font-[300]'>{company}</div>
+      <div className='w-full text-xl font-[200]'>{position}</div>
       <div className='w-full text-lg'>{years}</div>
       <div className='w-full'>{description}</div>
     </div>
